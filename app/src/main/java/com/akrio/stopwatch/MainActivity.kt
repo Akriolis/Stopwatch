376acc2aedc11e8980065b1ef6dfae5754f0e7fe
Akriolis <Akriolis@gmail.com>
2022-11-06 13:00:30 +0400

    Final

--- app/src/main/java/com/akrio/stopwatch/MainActivity.kt
+++ app/src/main/java/com/akrio/stopwatch/MainActivity.kt
@@ -8,13 +8,20 @@ import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        private const val OFFSET_KEY = "offset"
+        private const val RUNNING_KEY = "running"
+        private const val BASE_KEY = "base"
+    }
+
     private lateinit var startButton: Button
     private lateinit var pauseButton: Button
     private lateinit var resetButton: Button
 
-    lateinit var stopWatch: Chronometer
+    private lateinit var stopWatch: Chronometer
 
-    var offset: Long = 0
+    private var running = false
+    private var offset: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +30,16 @@ class MainActivity : AppCompatActivity() {
         startButton = findViewById(R.id.start_button)
         pauseButton = findViewById(R.id.pause_button)
         resetButton = findViewById(R.id.reset_button)
-
         stopWatch = findViewById(R.id.stopwatch)
 
-        var running = stopWatch.isCountDown
+        if (savedInstanceState != null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if (running){
+                stopWatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopWatch.start()
+            } else setBaseTime()
+        }
 
         startButton.setOnClickListener {
             if(!running){
@@ -47,6 +60,25 @@ class MainActivity : AppCompatActivity() {
         resetButton.setOnClickListener {
             offset = 0
             setBaseTime()
+            stopWatch.stop()
+            running = false
+        }
+    }
+
+    override fun onPause(){
+        super.onPause()
+        if(running){
+            saveOffSet()
+            stopWatch.stop()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(running){
+            setBaseTime()
+            stopWatch.start()
+            offset = 0
         }
     }
 
@@ -56,6 +88,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveOffSet(){
         offset = SystemClock.elapsedRealtime() - stopWatch.base
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(BASE_KEY,stopWatch.base)
+        super.onSaveInstanceState(outState)
     }
 
 }
